86fddd8fa28115e8bad602b2c165c43e5f7207b5
daniel03120 <daniel03120@126.com>
2025-12-19 15:10:13 +0800

    Update jammy.sh

--- config/suites/jammy.sh
+++ config/suites/jammy.sh
@@ -3,7 +3,7 @@
 export RELASE_NAME="Ubuntu 22.04 LTS (Jammy Jellyfish)"
 export RELASE_VERSION="22.04"
 
-export KERNEL_REPO="https://github.com/daniel03120/linux-5.10.y-rk35xx.git"
+export KERNEL_REPO="https://github.com/daniel03120/linux-rockchip.git"
 export KERNEL_BRANCH="jammy"
 export KERNEL_FLAVOR="rockchip"
 
